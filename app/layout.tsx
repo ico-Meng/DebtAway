ef4fc81d7e5977cabd1b5956c30baa022de8d87d
ico-Meng <ico.yigang.wang@gmail.com>
2026-03-12 18:59:29 -0400

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -178,6 +178,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2HVK989RQD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2HVK989RQD');
+          `}
+        </Script>
         <Script
           id="schema-organization"
           type="application/ld+json"
